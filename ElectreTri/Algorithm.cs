dd547487082b464c06e44d40194efd5efb023a6e
patryk <patrykbuzowicz@gmail.com>
2015-11-23 15:56:51 +0100

    init

---
+++ ElectreTri/Algorithm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectreTri
+{
+    class Algorithm
+    {
+        Result Solve(double[][] A, double[][] PP, double[] q, double[] p, double[] v, double[] w, double t)
+        {
+            var result = PrepareResult(A, PP);
+            var aColNumber = GetColNumber(A);
+            var ppColNumber = GetColNumber(PP);
+            var aRowNumber = GetRowNumber(A);
+
+            var c = GetCube(aColNumber, ppColNumber, aRowNumber);
+            var d = GetCube(aColNumber, ppColNumber, aRowNumber);
+
+            for (int ind = 0; ind < aRowNumber; ind++)
+            {
+                for (int i = 0; i < aColNumber; i++)
+                {
+                    for (int j = 0; j < ppColNumber; j++)
+                    {
+                        
+                    }
+                }   
+            }
+
+            return result;
+        }
+
+        private double[][][] GetCube(int x, int y, int z)
+        {
+            var result = new double[x][][];
+            for (var i = 0; i < x; i++)
+                result[i] = GetMatrix(y, z);
+
+            return result;
+        }
+
+        private int GetRowNumber(double[][] doubles)
+        {
+            return doubles.Length;
+        }
+
+        private Result PrepareResult(double[][] A, double[][] pp)
+        {
+            var result = new Result
+            {
+                Rank = GetMatrix(GetColNumber(A), GetColNumber(pp)),
+                Cs = GetMatrix(GetColNumber(A), GetColNumber(pp))
+            };
+            return result;
+        }
+
+        private double[][] GetMatrix(int rows, int columns)
+        {
+            var result = new double[rows][];
+
+            for (var i = 0; i < columns; i++)
+                result[i] = new double[columns];
+
+            return result;
+        }
+
+        private int GetColNumber(double[][] matrix)
+        {
+            return matrix.First().Length;
+        }
+    }
+
+    internal class Result
+    {
+        public double[][] Rank { get; set; }
+        public double[][] Cs { get; set; }
+    }
+}
